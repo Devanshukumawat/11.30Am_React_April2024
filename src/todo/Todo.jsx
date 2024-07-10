82718b7124f8bc2c2d73d04f46c8ade31374f70a
Devanshu <devanshukumawat063@gmail.com>
2024-07-10 12:33:36 +0530

    add

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -4,6 +4,9 @@ import TodoCss from "./todo.module.css"
 function Todo() {
 
     const [task,setTask] = useState("")
+    const [Ctask,setCtask] =useState(0)
+    const [Rtask,setRtask] =useState(0)
+    const [TotalTask,setTotaltask] =useState(0)
 
     const AllTask = [
         {taskName:"Buy Car", complete:true},
@@ -18,6 +21,7 @@ function Todo() {
 
     function handleForm(e){
         e.preventDefault()
+        console.log(task)
       
     }
 
@@ -26,13 +30,42 @@ function Todo() {
     }
 
     function addTask(){
-       
-        
+        if(task){
+        setTodo([...todo,{taskName:task,complete:false}])
+        }
+        setTask('')
+    }
+
+    function handleChecked(id){
+        const myArray = [...todo]
+        myArray[id].complete =! myArray[id].complete
+        setTodo(myArray)
+
+        const completeTask = myArray.filter((value,index)=>{
+           return value.complete
+        })
+
+        setCtask(completeTask.length)
+
+        const remainingTask = myArray.filter((value,index)=>{
+           return ! value.complete
+        })
+
+        setRtask(remainingTask.length)
+
+        const totaltasks = myArray.filter((value,index)=>{
+            return value
+        })
+
+        setTotaltask(totaltasks.length)
+
     }
 
     return ( 
         <>
+        <h4 style={{textAlign:"center"}}>Total Task:- {TotalTask} </h4>
             <div className={TodoCss.todo}>
+            
                 <div className={TodoCss.main}>
                 <h1>My Todo App 🙂</h1>
                 <div >
@@ -48,15 +81,17 @@ function Todo() {
 
                     todo.map((value,index)=>(
                         <ul className={TodoCss.list}>
-                           <span><li><input type="checkbox" checked={value.complete} /> {value.taskName} <i className="bi bi-trash3-fill float-end text-info"></i> <i className="bi bi-pencil-square float-end me-3 text-warning"></i> </li></span>
+                           <span><li><input type="checkbox" checked={value.complete}
+                           onClick={()=>{handleChecked(index)}}
+                            /> <span style={{textDecoration:value.complete ? "line-through" : ""}}>{value.taskName}</span> <i className="bi bi-trash3-fill float-end text-info"></i> <i className="bi bi-pencil-square float-end me-3 text-warning"></i> </li></span>
                         </ul>
                         
                     ))
                 }
                 </div>
                 <span>
-                    <p>Completed Task :- 2</p>
-                    <p>Remaining Task :- 1</p>
+                    <p>Completed Task :- {Ctask}</p>
+                    <p>Remaining Task :- {Rtask}</p>
                 </span>
                 </div>
                 
